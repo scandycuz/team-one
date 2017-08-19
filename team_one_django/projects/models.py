ba090330c826a9d6dfe495cf3dabdd3d3db7f14b
Joshua Ling <joshling1919@gmail.com>
2017-08-19 15:42:17 -0700

    Create projects model

--- team_one_django/projects/models.py
+++ team_one_django/projects/models.py
@@ -2,9 +2,12 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Project(models.Model):
-    points = models.IntegerField(default= 0)
+    points = models.IntegerField(default=0)
     title = models.CharField(max_length=100, blank=True, default='')
     desc = models.TextField()
+    admin = models.ForeignKey(User, related_name='admin', on_delete=models.SET_NULL, null=True)
+    user = models.ManyToManyField(User, related_name='user')
--- team_one_django/team_one_django/settings.py
+++ team_one_django/team_one_django/settings.py
@@ -31,14 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'projects.apps.ProjectsConfig',    
-    'rest_framework',    
+    'projects.apps.ProjectsConfig',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',    
+    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [

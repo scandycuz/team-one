fba53d0ec5aab484aa19f653dcce38a4d8b0d8da
scandycuz <trevorscandalios@gmail.com>
2017-08-19 19:34:00 -0700

    Adds routing

--- team_one_django/frontend/index.js
+++ team_one_django/frontend/index.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { Route, HashRouter } from 'react-router-dom';
 
 import configureStore from './store/Store';
 
-const Root = ({ store }) => (
-  <main>
-    { store.getState().session.user }
-    React is live!
-  </main>
-)
-
 document.addEventListener('DOMContentLoaded', () => {
   let store;
-  
+
+  class TempApp extends React.Component {
+    render() {
+      return (
+        <div>Hello World!</div>
+      );
+    }
+  }
+
   store = configureStore();
   const container = document.getElementById('container');
-  ReactDOM.render(<Root store={store} />, container);
+  ReactDOM.render(
+    <Provider store={store}>
+      <HashRouter>
+        <Route path="/" component={TempApp} />
+      </HashRouter>
+    </Provider>,
+    container
+  );
 });

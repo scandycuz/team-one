5fc883ca1abb009bdf349dd7882d6c171c992896
Sean Chen <sean.chen909@gmail.com>
2017-08-19 21:25:43 -0700

    filled header components

--- team_one_django/frontend/components/App.jsx
+++ team_one_django/frontend/components/App.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 import RaisedButton from 'material-ui/RaisedButton';
 import SideBar from './SideBar/SideBar';
+import Header from './Header/Header';
 
 const App = () => {
   return(
+    
     <div className="app">
+      <Header/>
       <SideBar />
     </div>
   );
---
+++ team_one_django/frontend/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React, {Component } from 'react';
+
+class Header extends Component {
+    render() {
+        var projectTitle = 'Team One';
+        return(
+            <div>
+                <h1>{projectTitle}</h1>
+            </div>
+        );
+    }
+}
+
+export default Header;
